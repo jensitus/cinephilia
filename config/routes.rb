02e41a05767c7682f1ed1de58b9207b713f138f7
jensitus <jens@ist-ur.org>
2025-08-20 20:47:47 +0200

    root to movies

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "posts#index"
+  # root "posts#index"
+  root "movies#index"
 end
