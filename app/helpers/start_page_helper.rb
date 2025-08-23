fb416e9fcd444d4406636d039b1255e1c59040f5
jensitus <jens@ist-ur.org>
2025-08-23 10:09:10 +0200

    unless text.nil?

--- app/helpers/start_page_helper.rb
+++ app/helpers/start_page_helper.rb
@@ -3,7 +3,7 @@ module StartPageHelper
   def truncate_and_link(text, options = {})
     length = options[:length] || 100
     url = options[:url] || '#'
-    return text if text.size <= length
+    return text if text.size <= length unless text.nil?
 
     output = raw(truncate(text, length: length))
     output += link_to(' more', url, class: 'more')
