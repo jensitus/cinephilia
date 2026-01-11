0a770f0903fda63a89f34a0060b9c84337da0387
jensitus <jens@ist-ur.org>
2026-01-11 07:46:08 +0100

    content for :title

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,14 @@
 class Genre < ApplicationRecord
-  include Searchable
+  # include Searchable
 
   has_and_belongs_to_many :movies
+
+  scope :search, ->(query) {
+    return none if query.blank?
+
+    where("search_vector @@ plainto_tsquery('german', ?)", query)
+      .order(Arel.sql("ts_rank(search_vector, plainto_tsquery('german', '#{sanitize_sql_like(query)}')) DESC"))
+  }
 
   def currently_showing_movies
     movies.currently_showing.order(:title)
