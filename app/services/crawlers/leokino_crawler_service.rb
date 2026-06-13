ff0b369049a9a24b5d9f482c266f19961fdc012b
jensitus <jens@ist-ur.org>
2026-06-13 08:56:56 +0200

    linti & scan

--- app/services/crawlers/leokino_crawler_service.rb
+++ app/services/crawlers/leokino_crawler_service.rb
@@ -1,40 +1,40 @@
 module Crawlers
   class LeokinoCrawlerService < BaseCrawlerService
-    CINEMA_URL   = "https://www.leokino.at"
+    CINEMA_URL = "https://www.leokino.at"
     PROGRAMS_URL = "https://www.leokino.at/ajax/programm.php"
-    VIENNA_TZ    = ActiveSupport::TimeZone["Vienna"]
+    VIENNA_TZ = ActiveSupport::TimeZone["Vienna"]
 
-    LEO_ID      = "t-leokino"
-    LEO_TITLE   = "Leokino"
-    LEO_COUNTY  = "Tirol"
-    LEO_STREET  = "Anichstraße 36"
-    LEO_ZIP     = "6020"
-    LEO_CITY    = "Innsbruck"
-    LEO_EMAIL   = "office@leokino.at"
-    LEO_PHONE   = "+43 512 56 04 70"
+    LEO_ID = "t-leokino"
+    LEO_TITLE = "Leokino"
+    LEO_COUNTY = "Tirol"
+    LEO_STREET = "Anichstraße 36"
+    LEO_ZIP = "6020"
+    LEO_CITY = "Innsbruck"
+    LEO_EMAIL = "office@leokino.at"
+    LEO_PHONE = "+43 512 56 04 70"
 
-    CINE_ID      = "t-cinematograph-innsbruck"
-    CINE_TITLE   = "Cinematograph Innsbruck"
-    CINE_COUNTY  = "Tirol"
-    CINE_STREET  = "Museumstraße 31"
-    CINE_ZIP     = "6020"
-    CINE_CITY    = "Innsbruck"
-    CINE_EMAIL   = "office@leokino.at"
-    CINE_PHONE   = "+43 512 56 04 70 50"
+    CINE_ID = "t-cinematograph-innsbruck"
+    CINE_TITLE = "Cinematograph Innsbruck"
+    CINE_COUNTY = "Tirol"
+    CINE_STREET = "Museumstraße 31"
+    CINE_ZIP = "6020"
+    CINE_CITY = "Innsbruck"
+    CINE_EMAIL = "office@leokino.at"
+    CINE_PHONE = "+43 512 56 04 70 50"
 
     SPECIAL_CLASSES = {
-      "buttonKinderfilm" => { name: "Junges Kino",    url: "https://www.leokino.at/programm/junges-kino/" },
+      "buttonKinderfilm" => { name: "Junges Kino", url: "https://www.leokino.at/programm/junges-kino/" },
       "buttonZeitreisen" => { name: "Kinozeitreisen", url: "https://www.leokino.at/festivals/kinozeitreisen/" },
-      "buttonFilmstart"  => { name: "Filmstart",      url: nil },
-      "buttonFestival"   => { name: "Festival",       url: nil }
+      "buttonFilmstart" => { name: "Filmstart", url: nil },
+      "buttonFestival" => { name: "Festival", url: nil }
     }.freeze
 
     def call
-      @leo_cinema      = find_or_create_cinema(id: LEO_ID,  title: LEO_TITLE,  county: LEO_COUNTY,  url: CINEMA_URL)
-      @cine_cinema     = find_or_create_cinema(id: CINE_ID, title: CINE_TITLE, county: CINE_COUNTY, url: CINEMA_URL)
+      @leo_cinema = find_or_create_cinema(id: LEO_ID, title: LEO_TITLE, county: LEO_COUNTY, url: CINEMA_URL)
+      @cine_cinema = find_or_create_cinema(id: CINE_ID, title: CINE_TITLE, county: CINE_COUNTY, url: CINEMA_URL)
       update_contacts
       @tag_descriptions = {}
-      @film_year_cache  = {}
+      @film_year_cache = {}
 
       (0...Cinephilia::Config::DAYS_TO_FETCH).each do |i|
         process_day(Date.today + i)
@@ -45,7 +45,7 @@ module Crawlers
     private
 
     def update_contacts
-      @leo_cinema.update(street: LEO_STREET,   zip: LEO_ZIP, city: LEO_CITY, telephone: LEO_PHONE,  email: LEO_EMAIL)
+      @leo_cinema.update(street: LEO_STREET, zip: LEO_ZIP, city: LEO_CITY, telephone: LEO_PHONE, email: LEO_EMAIL)
       @cine_cinema.update(street: CINE_STREET, zip: CINE_ZIP, city: CINE_CITY, telephone: CINE_PHONE, email: CINE_EMAIL)
     end
 
@@ -69,19 +69,21 @@ module Crawlers
       time = parse_time(date, time_node.text.strip)
       return unless time
 
-      cinema    = cinema_for(card)
-      h6_text   = card.at_css("h6.nomargin")&.text.to_s
-      director  = extract_director(h6_text)
-      ov_info   = if h6_text.include?("OmU")   then "OmU"
-                  elsif h6_text.match?(/\bOV\b/) then "OV"
-                  end
-      ov        = ov_info.present?
+      cinema = cinema_for(card)
+      h6_text = card.at_css("h6.nomargin")&.text.to_s
+      director = extract_director(h6_text)
+      ov_info = if h6_text.include?("OmU") then
+                  "OmU"
+                elsif h6_text.match?(/\bOV\b/) then
+                  "OV"
+                end
+      ov = ov_info.present?
 
       movie = find_or_create_movie(
-        display_title:  title,
+        display_title: title,
         original_title: title,
-        year:           date.year.to_s,
-        director_hint:  director
+        year: date.year.to_s,
+        director_hint: director
       )
       return unless movie
 
